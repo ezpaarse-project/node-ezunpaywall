3d3724833af9b00109cfab99ee2862991b1802aa
felixleo22 <leofelixoff@outlook.fr>
2020-10-12 14:20:56 +0200

    feat: enricher

--- bin/cmds/enricher.js
+++ bin/cmds/enricher.js
@@ -1,102 +1,240 @@
 const fs = require('fs-extra');
 const readline = require('readline');
 const path = require('path');
+const Papa = require('papaparse');
 const axios = require('../../lib/axios');
+
+let tabAttributes = [
+  'data_standard',
+  'doi',
+  'doi_url',
+  'genre',
+  'is_paratext',
+  'is_oa',
+  'journal_is_in_doaj',
+  'journal_is_oa',
+  'journal_issns',
+  'journal_issn_l',
+  'journal_name',
+  'oa_status',
+  'published_date',
+  'publisher',
+  'title',
+  'updated',
+  'year',
+];
+
+let headers = [];
+let i = '';
+
+const getExtension = (file) => {
+  const basename = file.split(/[\\/]/).pop();
+  const pos = basename.lastIndexOf('.');
+  if (basename === '' || pos < 1) {
+    return '';
+  }
+  return basename.slice(pos + 1);
+};
 
 const fetchEzUnpaywall = async (tab) => {
   let dois = [];
-  let unpaywallRes;
-  // map1 contain index of doi
-  const map1 = tab.map((elem) => elem?.doi);
-
-  // dois contain only doi to request ezunpaywall
-  dois = map1.filter((elem) => elem !== undefined);
+  let response = [];
+  // contain index of doi
+  const map1 = await tab.map((elem) => elem?.doi);
+  // contain array of doi to request ezunpaywall
+  dois = await map1.filter((elem) => elem !== undefined);
   try {
-    unpaywallRes = await axios({
+    response = await axios({
       method: 'post',
       url: '/graphql',
       data: {
-        query: 'query ($dois: [ID!]!) {getDatasUPW(dois: $dois) { doi, data_standard, genre, is_paratext, journal_issns, journal_issn_l, journal_name, published_date, publisher, title, year}}',
+        query: `query ($dois: [ID!]!) {getDatasUPW(dois: $dois) { doi, ${tabAttributes.toString()} }}`,
         variables: {
           dois,
         },
       },
     });
   } catch (err) {
-    console.log(err.response.error);
+    if (!err.response) {
+      console.log('error: url server incorrect');
+    }
     process.exit(1);
   }
-  dois = [];
-  return unpaywallRes;
+  // TODO verif
+  return response.data.data.getDatasUPW;
 };
 
-const enricher = (tab, unpaywallRes) => {
-  let found;
-  // enricher datas
-  tab.forEach((elem1) => {
-    if (elem1?.doi) {
-      found = unpaywallRes.data.data.getDatasUPW.find((elem2) => elem2.doi === elem1.doi);
-      if (found) {
-        elem1.upw = found;
-      }
+const enricherTab = (tab, response) => {
+  const results = new Map();
+  // index on doi
+  response.forEach((el) => {
+    if (el.doi) {
+      results.set(el.doi, el);
     }
+  });
+  // enricher
+  tab.forEach((el) => {
+    if (!el.doi) {
+      return;
+    }
+    const data = results.get(el.doi);
+    if (!data) {
+      return;
+    }
+    tabAttributes.forEach((attr) => {
+      if (attr !== 'doi') {
+        el[attr] = data[attr];
+      }
+    });
   });
 };
 
-const writeInFile = (tab) => {
+const writeInFileJSON = async (tab) => {
   try {
-    tab.forEach(async (enrichedData) => {
-      await fs.appendFile('test.jsonl', `${JSON.stringify(enrichedData)}\n`);
-    });
+    const stringTab = tab.map((el) => JSON.stringify(el)).join('\n');
+    await fs.appendFile('out.json', stringTab);
   } catch (err) {
     console.error(err);
   }
 };
 
+const readFileJSON = async (readStream) => {
+  const rl = readline.createInterface({
+    input: readStream,
+    crlfDelay: Infinity,
+  });
+
+  let tab = [];
+  let parsedLine;
+
+  // eslint-disable-next-line no-restricted-syntax
+  for await (const line of rl) {
+    parsedLine = JSON.parse(line);
+    if (tab.length !== 100) {
+      tab.push(parsedLine);
+    }
+    if (tab.length === 100) {
+      const response = await fetchEzUnpaywall(tab);
+      enricherTab(tab, response);
+      await writeInFileJSON(tab);
+      tab = [];
+    }
+  }
+  // last insertion
+  if (tab.length !== 0) {
+    const response = await fetchEzUnpaywall(tab);
+    enricherTab(tab, response);
+    await writeInFileJSON(tab);
+  }
+};
+
+const writeInFileCSV = async (tab) => {
+  try {
+    const val = await Papa.unparse(tab, {
+      header: false,
+      delimiter: ',',
+      columns: headers,
+    });
+    // TODO passé par un stream d'écriture writeFileStream
+    await fs.appendFile('out.csv', `${val}\r\n`);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const enricherHeaderCSV = async (header) => {
+  // delete attribute already in header
+  await header.forEach((el) => {
+    if (tabAttributes.includes(el)) {
+      const index = tabAttributes.indexOf(el);
+      tabAttributes.splice(index, 1);
+    }
+  });
+
+  // enricher header
+  return header.concat(tabAttributes);
+};
+
+const writeHeaderCSV = async (header) => {
+  try {
+    // TODO passé par un stream d'écriture writeFileStream
+    await fs.appendFile('out.csv', `${header}\r\n`);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const readFileCSV = (readStream) => {
+  let tab = [];
+  let head = true;
+  Papa.parse(readStream, {
+    delimiter: ',',
+    header: true,
+    transformHeader: (header) => {
+      headers.push(header.trim());
+      return header.trim();
+    },
+    step: async (results, parser) => {
+      // first step: write enriched header
+      if (head) {
+        head = false;
+        await parser.pause();
+        headers = await enricherHeaderCSV(headers);
+        await writeHeaderCSV(headers);
+        await parser.resume();
+      }
+      if (tab.length !== 100) {
+        tab.push(results.data);
+      }
+      if (tab.length === 100) {
+        const needEnriched = tab;
+        tab = [];
+        await parser.pause();
+        const response = await fetchEzUnpaywall(needEnriched);
+        enricherTab(needEnriched, response);
+        await writeInFileCSV(needEnriched);
+        await parser.resume();
+      }
+    },
+  });
+};
+
+const checkAttributes = (attributes) => {
+  if (attributes?.length) {
+    attributes.forEach((attr) => {
+      if (!tabAttributes.includes(attr)) {
+        console.log(`error: attribut ${attr} doesn't on unpaywall data`);
+        process.exit(1);
+      }
+    });
+    tabAttributes = attributes;
+  }
+};
+
 module.exports = {
   enricher: async (args) => {
+    const file = path.resolve(args.file);
     if (!args.file) {
       console.log('error: file expected');
       process.exit(1);
     }
-    const ifFileExist = await fs.pathExists(path.resolve(args.file));
+    const ifFileExist = await fs.pathExists(file);
     if (!ifFileExist) {
       console.log('error: file not found');
       process.exit(1);
     }
+    checkAttributes(args.attributes);
     let readStream;
     try {
-      readStream = fs.createReadStream(path.resolve(args.file));
+      readStream = fs.createReadStream(file);
     } catch (err) {
-      console.log(err);
+      console.log('error: impossible de read file');
     }
-
-    const rl = readline.createInterface({
-      input: readStream,
-      crlfDelay: Infinity,
-    });
-
-    let tab = [];
-    let unpaywallRes;
-
-    // eslint-disable-next-line no-restricted-syntax
-    for await (const line of rl) {
-      const parsedLine = JSON.parse(line);
-      if (tab.length !== 100) {
-        tab.push(parsedLine);
-      }
-      if (tab.length === 100) {
-        unpaywallRes = await fetchEzUnpaywall(tab);
-        enricher(tab, unpaywallRes);
-        writeInFile(tab);
-        tab = [];
-      }
+    if (getExtension(file) === 'jsonl') {
+      readFileJSON(readStream);
     }
-    // if data
-    if (tab.length !== 0) {
-      unpaywallRes = await fetchEzUnpaywall(tab);
-      enricher(tab, unpaywallRes);
-      writeInFile(tab);
+    if (getExtension(file) === 'csv') {
+      readFileCSV(readStream);
     }
   },
 };
