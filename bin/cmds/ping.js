3dce8c691bef041714b7e6d58c09379c411b95cf
Léo <49429942+felixleo22@users.noreply.github.com>
2021-06-14 14:54:43 +0200

    Revert "Revert "Ezu command""

--- bin/cmds/ping.js
+++ bin/cmds/ping.js
@@ -1,23 +1,27 @@
-const { connection, getConfig } = require('../../lib/axios');
-const logger = require('../../lib/logger');
+const { connection } = require('../../lib/axios');
+const { getConfig } = require('../../lib/config');
+
+/**
+ * check if service is available
+ * @param {*} args commander arguments
+ * @param -u --use <use> - use a custom config
+ */
+const ping = async (args) => {
+  const axios = await connection(args.use);
+  const config = await getConfig(args.use);
+  try {
+    await axios({
+      method: 'GET',
+      url: '/ping',
+    });
+  } catch (err) {
+    console.error(`service unavailable ${config.url}:${config.port}`);
+    process.exit(1);
+  }
+  console.log(`service available ${config.url}:${config.port}`);
+  process.exit(0);
+};
 
 module.exports = {
-  ping: async (args) => {
-    const axios = await connection(args.use);
-    const config = await getConfig(args.use);
-    let res;
-    try {
-      res = await axios({
-        method: 'GET',
-        url: '/ping',
-      });
-    } catch (err) {
-      logger.error(`service unavailable ${config.url}:${config.port}`);
-      process.exit(1);
-    }
-    if (res?.data?.data === 'pong') {
-      logger.info(`service available ${config.url}:${config.port}`);
-      process.exit(0);
-    }
-  },
+  ping,
 };
